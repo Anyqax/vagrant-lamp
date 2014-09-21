322e68ec9dbac7bb8885327d8d9e42d1c9baa7c1
Martin Menke <mail@martinmenke.net>
2014-09-21 15:00:43 +0200

    added mcrypt module fixed

--- provision.sh
+++ provision.sh
@@ -17,7 +17,8 @@ apt-get -y install build-essential binutils-doc git
 
 # Install Apache
 apt-get -y install apache2
-apt-get -y install php5 php5-curl php5-mysql php5-sqlite php5-xdebug
+apt-get -y install php5 php5-curl php5-mysql php5-sqlite php5-xdebug mcrypt
+php5enmod mcrypt
 
 sed -i "s/display_startup_errors = Off/display_startup_errors = On/g" ${php_config_file}
 sed -i "s/display_errors = Off/display_errors = On/g" ${php_config_file}
